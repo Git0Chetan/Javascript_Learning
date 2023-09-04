9fa4e18fa4b54c365c89dff912af47a5088c85cb
chetan <chetan249581@gmail.com>
2023-09-04 05:41:20 +0530

    Table js file is added

---
+++ lab/02_Table.js
@@ -0,0 +1,9 @@
+
+function compute(){
+    var a=document.getElementById("A").value;
+    var calculate=[];
+    for(let i=1;i<=10;i++){
+        calculate[i-1]= "<br>" + a + " x " + i + "=" + a*i ; 
+    }
+    document.getElementById("display").innerHTML= calculate;
+}
