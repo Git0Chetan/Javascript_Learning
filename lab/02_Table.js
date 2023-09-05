166ff4bbc821dcb5c9d7dac8f96bfb06e1c33d3d
chetan <chetan249581@gmail.com>
2023-09-05 20:05:22 +0530

    Dom insertion style changed in js

--- lab/02_Table.js
+++ lab/02_Table.js
@@ -1,9 +1,20 @@
 
+// function compute(){
+//     var a=document.getElementById("A").value;
+//     var calculate=[];
+//     for(let i=1;i<=10;i++){
+//         calculate[i-1]= "<br>" + a + " x " + i + "=" + a*i ; 
+//     }
+//     document.getElementById("display").innerHTML += calculate;
+// }
+
+
 function compute(){
     var a=document.getElementById("A").value;
-    var calculate=[];
+    var calculate;
     for(let i=1;i<=10;i++){
-        calculate[i-1]= "<br>" + a + " x " + i + "=" + a*i ; 
+        calculate= "<br>" + a + " x " + i + "=" + a*i ; 
+        document.getElementById("display").innerHTML += calculate;
     }
-    document.getElementById("display").innerHTML= calculate;
+    
 }
